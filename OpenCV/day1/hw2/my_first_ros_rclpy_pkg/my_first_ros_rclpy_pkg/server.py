b818a8a4d4101eab48e677c105b0660abaab3b36
kimminjo123 <mjkim8820@gmail.com>
2025-09-19 16:34:26 +0900

    Add files via upload

---
+++ OpenCV/day1/hw2/my_first_ros_rclpy_pkg/my_first_ros_rclpy_pkg/server.py
@@ -0,0 +1,20 @@
+import socket
+import numpy
+import cv2
+
+def main():
+    UDP_IP = "172.100.4.74"
+    UDP_PORT = 9505
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
+
+    print("Waiting photo")
+
+    data, addr = sock.recvfrom(65535)
+    photo = numpy.fromstring(data, dtype=numpy.uint8)
+    photo_2 = cv2.imdecode(photo, cv2.IMREAD_COLOR)
+
+    cv2.imshow("Receive photo", photo_2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
